55171e6be2185de34f0d99cf97b0c5c89e42a964
antonydp <38143733+antonydp@users.noreply.github.com>
2024-10-26 15:23:24 +0200

    add files

---
+++ src/components/testimonial17.js
@@ -0,0 +1,279 @@
+import React, { Fragment } from 'react'
+
+import PropTypes from 'prop-types'
+
+import './testimonial17.css'
+
+const Testimonial17 = (props) => {
+  return (
+    <div className="thq-section-padding">
+      <div className="testimonial17-max-width thq-section-max-width">
+        <div className="testimonial17-container10">
+          <h2>
+            {props.heading1 ?? (
+              <Fragment>
+                <h2 className="testimonial17-text35 thq-heading-2">
+                  Testimonianze
+                </h2>
+              </Fragment>
+            )}
+          </h2>
+          <span>
+            {props.content1 ?? (
+              <Fragment>
+                <span className="testimonial17-text32 thq-body-small">
+                  Sono rimasto estremamente soddisfatto del lavoro svolto da APS
+                  Pietra Maiella. La loro attenzione ai dettagli e la maestria
+                  nel lavorare la pietra sono davvero impressionanti.
+                </span>
+              </Fragment>
+            )}
+          </span>
+        </div>
+        <div className="thq-grid-2">
+          <div className="thq-animated-card-bg-2">
+            <div className="thq-animated-card-bg-1">
+              <div
+                data-animated="true"
+                className="thq-card testimonial17-card1"
+              >
+                <div className="testimonial17-container12">
+                  <img
+                    alt={props.author1Alt}
+                    src={props.author1Src}
+                    className="testimonial17-image1"
+                  />
+                  <div className="testimonial17-container13">
+                    <strong>
+                      {props.author1Name ?? (
+                        <Fragment>
+                          <strong className="testimonial17-text26 thq-body-large">
+                            Giovanni Rossi
+                          </strong>
+                        </Fragment>
+                      )}
+                    </strong>
+                    <span>
+                      {props.author1Position ?? (
+                        <Fragment>
+                          <span className="testimonial17-text25 thq-body-small">
+                            Architetto
+                          </span>
+                        </Fragment>
+                      )}
+                    </span>
+                  </div>
+                </div>
+                <span>
+                  {props.review1 ?? (
+                    <Fragment>
+                      <span className="testimonial17-text34 thq-body-small">
+                        5 stelle
+                      </span>
+                    </Fragment>
+                  )}
+                </span>
+              </div>
+            </div>
+          </div>
+          <div className="thq-animated-card-bg-2">
+            <div className="thq-animated-card-bg-1">
+              <div
+                data-animated="true"
+                className="thq-card testimonial17-card2"
+              >
+                <div className="testimonial17-container14">
+                  <img
+                    alt={props.author2Alt}
+                    src={props.author2Src}
+                    className="testimonial17-image2"
+                  />
+                  <div className="testimonial17-container15">
+                    <strong>
+                      {props.author2Name ?? (
+                        <Fragment>
+                          <strong className="testimonial17-text29 thq-body-large">
+                            Maria Bianchi
+                          </strong>
+                        </Fragment>
+                      )}
+                    </strong>
+                    <span>
+                      {props.author2Position ?? (
+                        <Fragment>
+                          <span className="testimonial17-text24 thq-body-small">
+                            Interior Designer
+                          </span>
+                        </Fragment>
+                      )}
+                    </span>
+                  </div>
+                </div>
+                <span>
+                  {props.review2 ?? (
+                    <Fragment>
+                      <span className="testimonial17-text28 thq-body-small">
+                        Il team di APS Pietra Maiella ha realizzato un progetto
+                        su misura per le mie esigenze. Professionali e puntuali,
+                        li consiglio vivamente.
+                      </span>
+                    </Fragment>
+                  )}
+                </span>
+              </div>
+            </div>
+          </div>
+          <div className="thq-animated-card-bg-2">
+            <div className="thq-animated-card-bg-1">
+              <div
+                data-animated="true"
+                className="thq-card testimonial17-card3"
+              >
+                <div className="testimonial17-container16">
+                  <img
+                    alt={props.author3Alt}
+                    src={props.author3Src}
+                    className="testimonial17-image3"
+                  />
+                  <div className="testimonial17-container17">
+                    <strong>
+                      {props.author3Name ?? (
+                        <Fragment>
+                          <strong className="testimonial17-text27 thq-body-large">
+                            Luca Verdi
+                          </strong>
+                        </Fragment>
+                      )}
+                    </strong>
+                    <span>
+                      {props.author3Position ?? (
+                        <Fragment>
+                          <span className="testimonial17-text33 thq-body-small">
+                            Imprenditore
+                          </span>
+                        </Fragment>
+                      )}
+                    </span>
+                  </div>
+                </div>
+                <span>
+                  {props.review3 ?? (
+                    <Fragment>
+                      <span className="testimonial17-text36 thq-body-small">
+                        Servizio impeccabile e risultati sorprendenti. APS
+                        Pietra Maiella ha trasformato la mia idea in realtà con
+                        competenza e passione.
+                      </span>
+                    </Fragment>
+                  )}
+                </span>
+              </div>
+            </div>
+          </div>
+          <div className="thq-animated-card-bg-2">
+            <div className="thq-animated-card-bg-1">
+              <div
+                data-animated="true"
+                className="thq-card testimonial17-card4"
+              >
+                <div className="testimonial17-container18">
+                  <img
+                    alt={props.author4Alt}
+                    src={props.author4Src}
+                    className="testimonial17-image4"
+                  />
+                  <div className="testimonial17-container19">
+                    <strong>
+                      {props.author4Name ?? (
+                        <Fragment>
+                          <strong className="testimonial17-text31 thq-body-large">
+                            Elena Rossetti
+                          </strong>
+                        </Fragment>
+                      )}
+                    </strong>
+                    <span>
+                      {props.author4Position ?? (
+                        <Fragment>
+                          <span className="testimonial17-text30 thq-body-small">
+                            Restauratrice
+                          </span>
+                        </Fragment>
+                      )}
+                    </span>
+                  </div>
+                </div>
+                <span>
+                  {props.review4 ?? (
+                    <Fragment>
+                      <span className="testimonial17-text37 thq-body-small">
+                        Professionisti eccezionali nel settore della lavorazione
+                        della pietra. Consiglio vivamente il loro lavoro.
+                      </span>
+                    </Fragment>
+                  )}
+                </span>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+Testimonial17.defaultProps = {
+  author2Position: undefined,
+  author1Position: undefined,
+  author3Alt: 'Immagine di Luca Verdi',
+  author1Name: undefined,
+  author1Src:
+    'https://images.unsplash.com/photo-1632324343640-86af9827dbeb?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w5MTMyMXwwfDF8cmFuZG9tfHx8fHx8fHx8MTcyOTk0ODU3MXw&ixlib=rb-4.0.3&q=80&w=1080',
+  author3Name: undefined,
+  review2: undefined,
+  author2Name: undefined,
+  author4Position: undefined,
+  author4Name: undefined,
+  author4Src:
+    'https://images.unsplash.com/photo-1620853724625-4715441de25d?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w5MTMyMXwwfDF8cmFuZG9tfHx8fHx8fHx8MTcyOTk0ODU3MXw&ixlib=rb-4.0.3&q=80&w=1080',
+  author1Alt: 'Immagine di Giovanni Rossi',
+  author2Src:
+    'https://images.unsplash.com/photo-1529139574466-a303027c1d8b?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w5MTMyMXwwfDF8cmFuZG9tfHx8fHx8fHx8MTcyOTk0ODU3Mnw&ixlib=rb-4.0.3&q=80&w=1080',
+  author3Src:
+    'https://images.unsplash.com/photo-1533939141733-63078e139c4a?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w5MTMyMXwwfDF8cmFuZG9tfHx8fHx8fHx8MTcyOTk0ODU3Mnw&ixlib=rb-4.0.3&q=80&w=1080',
+  author2Alt: 'Immagine di Maria Bianchi',
+  author4Alt: 'Immagine di Elena Rossetti',
+  content1: undefined,
+  author3Position: undefined,
+  review1: undefined,
+  heading1: undefined,
+  review3: undefined,
+  review4: undefined,
+}
+
+Testimonial17.propTypes = {
+  author2Position: PropTypes.element,
+  author1Position: PropTypes.element,
+  author3Alt: PropTypes.string,
+  author1Name: PropTypes.element,
+  author1Src: PropTypes.string,
+  author3Name: PropTypes.element,
+  review2: PropTypes.element,
+  author2Name: PropTypes.element,
+  author4Position: PropTypes.element,
+  author4Name: PropTypes.element,
+  author4Src: PropTypes.string,
+  author1Alt: PropTypes.string,
+  author2Src: PropTypes.string,
+  author3Src: PropTypes.string,
+  author2Alt: PropTypes.string,
+  author4Alt: PropTypes.string,
+  content1: PropTypes.element,
+  author3Position: PropTypes.element,
+  review1: PropTypes.element,
+  heading1: PropTypes.element,
+  review3: PropTypes.element,
+  review4: PropTypes.element,
+}
+
+export default Testimonial17
